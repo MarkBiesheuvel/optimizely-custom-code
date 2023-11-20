4719432602c8267c5dec36501d2a4f4c0c28da87
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-11-20 12:16:09 +0100

    Add simple example of custom attributes

---
+++ project-javascript/attributes.js
@@ -0,0 +1,10 @@
+// Initialize the Optimizely global variable
+window.optimizely = window.optimizely || [];
+
+// Set hostname attribute
+window.optimizely.push({
+    type: 'user',
+    attributes: {
+        hostname: window.location.hostname
+    }
+});
