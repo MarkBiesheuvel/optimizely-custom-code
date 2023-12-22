f35e6c4ceb0afd90a42b8c9e4de228f80f102aae
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-12-22 13:13:12 +0100

    Add another example of custom attributes

---
+++ project-javascript/attributes-from-search.js
@@ -0,0 +1,14 @@
+// Initialize the Optimizely global variable
+window.optimizely = window.optimizely || [];
+
+// Parse query parameters
+const search = new URLSearchParams(window.location.search);
+
+// Set source and campaign attribute
+window.optimizely.push({
+    type: 'user',
+    attributes: {
+        pxl: search.get('pxl'), // source
+        plid: search.get('plid') // campaign
+    }
+});
