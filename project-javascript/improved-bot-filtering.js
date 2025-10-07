27d344a0c44812ce1bb4b3cbea52ccc0e0f3018c
Mark Biesheuvel <mail@markbiesheuvel.nl>
2025-10-07 10:12:41 +0200

    Add bot filtering script

---
+++ project-javascript/improved-bot-filtering.js
@@ -0,0 +1,26 @@
+// Initialize the Optimizely global variable
+window.optimizely = window.optimizely || [];
+
+// List of automated website testers
+const botList = [
+  'Chrome-Lighthouse',
+  'DatadogSynthetics',
+  'Googlebot',
+  'Speed Insights',
+  'WebPageTest',
+  'HeadlessChrome',
+  'Pingdom',
+  'GTmetrix',
+  'PhantomJS'
+];
+
+// Iterate through list of bots
+botList.forEach((bot) => {
+  // Check whether the current user agent includes a bot indicator
+  if (window.navigator.userAgent.includes(bot)) {
+    // Disabling tracking
+    window.optimizely.push({
+      type: 'holdEvents',
+    });
+  }
+});
